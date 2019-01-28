bdef13c882216548d45bb8fcc8570fad7cf404fe
chandryou <applegna@gmail.com>
2019-01-28 17:44:34 +0900

    first draft for Argos package

---
+++ extras/codeToDevelop.r
@@ -0,0 +1,4 @@
+#Make a package in the existing local git directory.
+package.skeleton(name = "Argos", encoding = "UTF-8",path = "C:/Users/apple/git/ABMI", force=TRUE)
+#devtools::create_description()
+usethis::use_description()
