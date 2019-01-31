b61002f5d7e77f5cc05a127886d810b7e0949f2f
chandryou <applegna@gmail.com>
2019-01-31 15:44:46 +0900

    update cost sql for cohort start year

--- inst/sql/sql_server/extractVisitCost.sql
+++ inst/sql/sql_server/extractVisitCost.sql
@@ -1,4 +1,4 @@
-SELECT cohort_definition_id, DATEDIFF(@min_date_unit,COHORT_START_DATE,visit.visit_start_date) as date_unit, visit.visit_concept_id, COUNT(visit.visit_occurrence_id) AS visit_count, COUNT(DISTINCT cohort.subject_id) AS subject_count, SUM(cost.paid_by_payer) AS paid_by_payer_sum, SUM(cost.paid_by_patient) AS paid_by_patient_sum, SUM(cost.total_charge) AS total_charge_sum--,concept.concept_name as visit_concept_name
+SELECT cohort_definition_id, YEAR(cohort_start_date) as cohort_start_year,DATEDIFF(@min_date_unit,COHORT_START_DATE,visit.visit_start_date) as date_unit, visit.visit_concept_id, COUNT(visit.visit_occurrence_id) AS visit_count, COUNT(DISTINCT cohort.subject_id) AS subject_count, SUM(cost.paid_by_payer) AS paid_by_payer_sum, SUM(cost.paid_by_patient) AS paid_by_patient_sum, SUM(cost.total_charge) AS total_charge_sum--,concept.concept_name as visit_concept_name
     FROM @target_database_schema.@target_cohort_table cohort
     INNER JOIN @cdm_database_schema.VISIT_OCCURRENCE visit
         ON cohort.subject_id = visit.person_id
@@ -19,5 +19,5 @@ SELECT cohort_definition_id, DATEDIFF(@min_date_unit,COHORT_START_DATE,visit.vis
                                                                   AND condition.condition_start_date >= DATEADD(DAY,@cost_window_start,cohort.cohort_start_date)
         														  AND condition.condition_start_date <= DATEADD(DAY,@cost_window_end,cohort.cohort_end_date))
                                                                 }
-    GROUP BY cohort_definition_id, DATEDIFF(@min_date_unit,COHORT_START_DATE,visit.visit_start_date), visit.visit_concept_id --, concept.concept_name
+    GROUP BY cohort_definition_id, YEAR(cohort_start_date),DATEDIFF(@min_date_unit,COHORT_START_DATE,visit.visit_start_date), visit.visit_concept_id --, concept.concept_name
     ;
