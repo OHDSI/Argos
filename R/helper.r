ca9a789a5cb81c1019db7932acc51e8856bfca4d
chandryou <applegna@gmail.com>
2019-02-02 18:10:19 +0900

    add MapCovariates function

--- R/helper.r
+++ R/helper.r
@@ -200,6 +200,38 @@ calculateNumberPerCovTime <- function(plpData,
     return(resultData)
 }
 
+# restricts to pop and saves/creates mapping
+MapCovariates <- function(covariates, covariateRef, population, map){
+    
+    # restrict to population for speed
+    OhdsiRTools::logTrace('restricting to population for speed...')
+    idx <- ffbase::ffmatch(x = covariates$rowId, table = ff::as.ff(population$rowId))
+    idx <- ffbase::ffwhich(idx, !is.na(idx))
+    covariates <- covariates[idx, ]
+    
+    OhdsiRTools::logTrace('Now converting covariateId...')
+    oldIds <- as.double(ff::as.ram(covariateRef$covariateId))
+    newIds <- 1:nrow(covariateRef)
+    
+    if(!is.null(map)){
+        OhdsiRTools::logTrace('restricting to model variables...')
+        OhdsiRTools::logTrace(paste0('oldIds: ',length(map[,'oldIds'])))
+        OhdsiRTools::logTrace(paste0('newIds:', max(as.double(map[,'newIds']))))
+        ind <- ffbase::ffmatch(x=covariateRef$covariateId, table=ff::as.ff(as.double(map[,'oldIds'])))
+        ind <- ffbase::ffwhich(ind, !is.na(ind))
+        covariateRef <- covariateRef[ind,]
+        
+        ind <- ffbase::ffmatch(x=covariates$covariateId, table=ff::as.ff(as.double(map[,'oldIds'])))
+        ind <- ffbase::ffwhich(ind, !is.na(ind))
+        covariates <- covariates[ind,]
+    }
+    if(is.null(map))
+        map <- data.frame(oldIds=oldIds, newIds=newIds)
+    
+    return(list(covariates=covariates,
+                covariateRef=covariateRef,
+                map=map))
+}
 
 ##Helper function (calculates integral)
 f<-function(x,ageWeighting,C = 0.1658, beta = 0.04, discount, age){
