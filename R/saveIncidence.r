44d7ca4fe9f01e757d511f49e785cc840e51522b
ParkYouJin <dbwls5223@naver.com>
2019-02-18 20:52:04 +0900

    make dir and saving tables and plots

---
+++ R/saveIncidence.r
@@ -0,0 +1,29 @@
+# Copyright 2019 Observational Health Data Sciences and Informatics
+#
+# This file is part of Argos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#'@import ggplot2
+#'@export
+## save incidence talbe and incidence proportion plot
+
+saveIncidence<-function(outputFolder,
+                      imageExtension = "png"){
+    dir.create(file.path(outputFolder, "incidence"))
+    incidenceFolder<-paste(outputFolder, "incidence", sep = "/")
+    saveRDS(incCal,file.path(incidenceFolder,paste0("incidenceCalData_cohortId_",cancerList$cohortId[i], ".rds" )))
+    write.csv(incCal,file.path(outputFolder,paste0("incidenceCalData_cohortId_",cancerList$cohortId[i], ".csv" )))
+    ggplot2::ggsave(file.path(outputFolder, paste0(fileName,".",imageExtension) ), bybirthPlot, width = 30,height = 15,units = "cm" )  
+    ggplot2::ggsave(file.path(outputFolder, paste0(ageSpefileName,".",imageExtension) ),ageSpePlot,  width = 30,height = 15,units = "cm" ) 
+    ggplot2::ggsave(file.path(outputFolder, paste0(ageAdjfileName,".",imageExtension) ), ageAdjPlot, width = 30,height = 15,units = "cm" ) 
+}
